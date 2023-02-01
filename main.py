81bc3c17dac0f3f8f2c559bcd6baacd88dc25a23
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-02-01 09:45:40 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import re
+import nltk
+from nltk.corpus import wordnet
+
+def get_synonyms(word):
+    synonyms = set()
+    for syn in wordnet.synsets(word):
+        for lemma in syn.lemmas():
+            synonym = lemma.name().replace("_", " ").replace("-", " ").lower()
+            synonyms.add(synonym)
+    return list(synonyms)
+
+def paraphrase_word(word):
+    synonyms = get_synonyms(word)
+    if len(synonyms) > 0:
+        return synonyms[0]
+    else:
+        return word
+
+def paraphrase_text(text):
+    words = nltk.word_tokenize(text)
+    paraphrased_words = [paraphrase_word(word) for word in words]
+    paraphrased_text = " ".join(paraphrased_words)
+    return paraphrased_text
+
+with open("input.txt", "r") as input_file:
+    text = input_file.read()
+
+paraphrased_text = paraphrase_text(text)
+
+with open("output.txt", "w") as output_file:
+    output_file.write(paraphrased_text)
